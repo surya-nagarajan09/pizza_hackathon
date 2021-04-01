63fad21976522752f9b489a9b2a5d51382ed0a84
surya-nagrajan <suryaadam321@gmail.com>
2021-04-02 02:08:12 +0530

    2 files modified

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./style.css";
 
 function Menu() {
   const [product, setProduct] = useState([]);
@@ -11,18 +12,24 @@ function Menu() {
     });
   }, []);
   return (
-    <div>
-    <ul>{product.map(x=>(
-      <div className="display">
-       <li key={x.id}><img  src={x.url}/></li>
-      <li key={x.id}>{x.name}</li>
-      <li key={x.id}>{x.price}</li>
-      <li key={x.id}>{x.size}</li>
+    <div className="display">
+      <div className="each">
+        {product.map(x => (
+          <div className="display">
+            <p key={x.id}>
+              <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwestdale.lavapizza.ca%2Fwp-content%2Fuploads%2F2017%2F01%2Fsingle-pizza-pic.png&f=1&nofb=1" />
+            </p>
+            <span key={x.id}>Name:{x.name}</span>
+            <br />
+            <span key={x.id}>price:{x.price}</span>
+            <br />
+            <span key={x.id}>Size:{x.size}</span>
+            <p key={x.id}>
+              <button>Add</button>
+            </p>
+          </div>
+        ))}
       </div>
-      
-    ))
-    }
-    </ul>
     </div>
   );
 }
