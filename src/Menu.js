5fe3ded5dc8a8315e0c70851b057f30b761c9b07
surya-nagrajan <suryaadam321@gmail.com>
2021-04-02 01:36:49 +0530

    1 file modified

--- src/Menu.js
+++ src/Menu.js
@@ -2,30 +2,28 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Menu() {
-  const [product,setProduct] = useState([]);
- 
+  const [product, setProduct] = useState([]);
+
   useEffect(() => {
-    axios.get('https://producstsss.herokuapp.com/').then(res => {
+    axios.get("https://producstsss.herokuapp.com/").then(res => {
       setProduct(res.data);
-      console.log(res.data)
+      console.log(res.data);
     });
   }, []);
   return (
     <div>
-      <div>{
-        product.map((x)=>(
-          <div>
-          <h2>{product.name}</h2>
-          <p>{product.price}</p>
-          <img src={product.url}/>
-          <p> {product.size}</p>
-        
+      <div>
+        {product.map(x => (
+          <div key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.price}</p>
+            <img src={product.url} />
+            <p> {product.size}</p>
           </div>
-        ))
-      }
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default Menu;
