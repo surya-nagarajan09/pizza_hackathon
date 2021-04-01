79a71665c94f8902c41190cfdf14d76471b562f5
surya-nagrajan <suryaadam321@gmail.com>
2021-04-02 00:01:16 +0530

    1 file modified

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function Main() {
-  return <p> from Menu</p>;
+function Menu() {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://producstsss.herokuapp.com/").then(res => {
+      setUserList(res.data.data);
+    });
+  }, []);
+  return <div />;
 }
 
-export default Main;
+export default Menu;
