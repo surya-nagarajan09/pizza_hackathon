a5b9b3e0898a513cf17319fb9a524bc0c940f8dc
surya-nagrajan <suryaadam321@gmail.com>
2021-04-02 01:21:47 +0530

    2 files modified

--- src/Nav.js
+++ src/Nav.js
@@ -1,40 +1,35 @@
 import React from "react";
 import "./style.css";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <div>
+    <div className="top">
       <ul className="list">
-       <Link to="/">
-        <li>Home</li>
-      </Link>
-      <Link to="/Menu">
-        <li>Menu</li>
-      </Link>
-      
-    
-      
-      
-       <Link to="/Custom">
-     <li>Make Pizza</li>
-      </Link>
-      <Link to="/Order">
-       <li>My Orders</li>
-      </Link>
-      <Link to="/Signin">
-       <li>Sign in</li>
-      </Link>
-      <Link to="/Signup">
-       <li>Sign up</li>
-      </Link>
-      
-      
-      
-       
-        
-       
-        
+        <img
+          src="https://i.postimg.cc/tJymSkN5/Pngtree-pizza-illustration-in-icon-drawn-5385559.png"
+          width="30"
+          height="30"
+        />
+        <Link to="/">
+          <li className="hlo">Home</li>
+        </Link>
+        <Link to="/Menu">
+          <li className="hlo">Menu</li>
+        </Link>
+
+        <Link to="/Custom">
+          <li className="hlo">Make Pizza</li>
+        </Link>
+        <Link to="/Order">
+          <li className="hlo">My Orders</li>
+        </Link>
+        <Link to="/Signin">
+          <li className="hlo">Sign in</li>
+        </Link>
+        <Link to="/Signup">
+          <li className="hlo">Sign up</li>
+        </Link>
       </ul>
     </div>
   );
