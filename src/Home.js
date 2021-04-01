4d29b41e9819bf1dd75c8499558eae5da98a772d
surya-nagrajan <suryaadam321@gmail.com>
2021-04-02 00:41:48 +0530

    1 file modified

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,18 @@ import "./style.css";
 
 function Home() {
   return (
-    <div className='home'>
-    <p>welcome</p>
-   
+    <div className="home">
+      <img
+        src="https://free4kwallpapers.com/uploads/originals/2017/04/09/pizza-in-4k-wallpaper.jpg"
+        height="600"
+        width="1500"
+      />
+      <div className="center">
+        <p>View collections in menu</p>
+        <p>Customize your pizzza here!!!</p>
+        <p />
+      </div>
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.7p4jF0-ONmDigjFHCupKpwAAAA%26pid%3DApi&f=1" />
     </div>
   );
 }
