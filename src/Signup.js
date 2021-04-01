596253f38f200c789fc44605cf3eb69a0a4fae04
surya-nagrajan <suryaadam321@gmail.com>
2021-04-01 19:22:23 +0530

    1 file modified

--- src/Signup.js
+++ src/Signup.js
@@ -1,23 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Signup() {
+  const url = "";
+  const [data, setData] = useState({
+    first_name: "",
+    last_name: "",
+    address: "",
+    code: "",
+    email: "",
+    passcode: "",
+    phonenumber: ""
+  });
+  function handle(e) {
+    const newdata = { ...data };
+    newdata[e.target.id] = e.target.value;
+    setData(newdata);
+    console.log(newdata);
+  }
   return (
     <div>
       <form>
         <p>first Name</p>
-        <input type="text" placeholder="enter first name" />
+        <input
+          onChange={e => handle(e)}
+          id="first_name"
+          value={data.first_name}
+          type="text"
+          placeholder="enter first name"
+        />
         <p>Last Name</p>
-        <input type="text" placeholder="enter Last name" />
+        <input
+          onChange={e => handle(e)}
+          id="last_name"
+          value={data.last_name}
+          type="text"
+          placeholder="enter Last name"
+        />
         <p>address </p>
-        <input type="text" placeholder="enter adress" />
+        <input
+          type="text"
+          onChange={e => handle(e)}
+          id="address"
+          value={data.address}
+          placeholder="enter adress"
+        />
         <label>Zip code</label>
-        <input type="number" placeholder="Enter code" />
+        <input
+          type="number"
+          placeholder="Enter code"
+          onChange={e => handle(e)}
+          id="code"
+          value={data.code}
+        />
         <p>Email adress</p>
-        <input placeholder="Enter email" />
+        <input
+          placeholder="Enter email"
+          onChange={e => handle(e)}
+          id="email"
+          value={data.email}
+        />
         <p>Passcode</p>
-        <input type="password" placeholder="Enter passcode" />
+        <input
+          type="password"
+          placeholder="Enter passcode"
+          onChange={e => handle(e)}
+          id="passcode"
+          value={data.passcode}
+        />
         <p>Phone number</p>
-        <input type="number" placeholder="Enter mobile number" />
+        <input
+          type="number"
+          placeholder="Enter mobile number"
+          onChange={e => handle(e)}
+          id="phonenumber"
+          value={data.phonenumber}
+        />
       </form>
       <div>
         <button>Sign up</button>
