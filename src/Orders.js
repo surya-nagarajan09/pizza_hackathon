16277c114daf21671aa3d827ae8179f692fbae36
surya-nagrajan <suryaadam321@gmail.com>
2021-04-02 09:50:47 +0530

    1 file modified

--- src/Orders.js
+++ src/Orders.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./style.css";
 
 function Order() {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://producstsss.herokuapp.com/").then(res => {
+      setProduct(res.data);
+      console.log(res.data);
+    });
+  }, []);
   return (
-    <div>
-      <p NameClass="empty">empty!!</p>
+    <div className="display">
+      <div className="each">
+        {product.map(x => (
+          <div className="display">
+            <span key={x.id}>Name:{x.base}</span>
+            <br />
+            <span key={x.id}>price:{x.sauce}</span>
+            <br />
+            <span key={x.id}>Size:{x.cheese}</span>
+            <p key={x.id}>
+              <button className="button">Add</button>
+            </p>
+         
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
